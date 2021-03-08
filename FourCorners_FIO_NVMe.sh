f3811261900c24492b56383e79e18946e6dd8132
anrahman4 <70340275+anrahman4@users.noreply.github.com>
2021-03-08 14:31:19 -0800

    Update FourCorners_FIO_NVMe.sh

--- FourCorners_FIO_NVMe.sh
+++ FourCorners_FIO_NVMe.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 #Usage: ./FourCorners_FIO_NVME.sh
 
-read -p "Which drive should benchmark use? Existing data will be lost! [default 'nvme0n1']: " NVMEDRIVE
-NVMEDRIVE=${NVMEDRIVE:-'nvme0n1'}
+#read -p "Which drive should benchmark use? Existing data will be lost! [default 'nvme0n1']: " NVMEDRIVE
+#NVMEDRIVE=${NVMEDRIVE:-'nvme0n1'}
+
+NVMEDRIVE=$1
 echo "Benchmark Drive: $NVMEDRIVE"
 
 testpath=/dev/$NVMEDRIVE
