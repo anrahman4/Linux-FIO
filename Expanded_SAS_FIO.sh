c9f943efc4407ae414d578b726b8cc9429a49528
anrahman4 <70340275+anrahman4@users.noreply.github.com>
2021-03-29 10:59:52 -0700

    Update Expanded_SAS_FIO.sh

--- Expanded_SAS_FIO.sh
+++ Expanded_SAS_FIO.sh
@@ -1,42 +1,34 @@
 #!/bin/bash
-#Usage: ./Expanded_NVMe_FIO.sh
+#Usage: ./Expanded_SAS_FIO.sh
 
-#read -p "Which drive should benchmark use? Existing data will be lost! [default 'nvme0n1']: " NVMEDRIVE
-#NVMEDRIVE=${NVMEDRIVE:-'nvme10n1'}
+#read -p "Which drive should benchmark use? Existing data will be lost! [default 'sde']: " SASDRIVE
+#SASDRIVE=${SASDRIVE:-'sde'}
 
-NVMEDRIVE=$1
-echo "Benchmark Drive: $NVMEDRIVE"
+SASDRIVE=$1
+echo "Benchmark Drive: $SASDRIVE"
 
-testpath=/dev/$NVMEDRIVE
+testpath=/dev/$SASDRIVE
 echo $testpath
 
 server_model=`sudo dmidecode -t1 | grep 'Product Name:' | xargs | cut -d ':' -f 2 | xargs | tr " " - | xargs`
 cpu_model=`sudo cat /proc/cpuinfo | grep 'model name' | uniq | cut -d ':' -f 2 | xargs | tr " " - | tr "@" a | tr "(" - | tr ")" - | xargs`
-serial_num=`nvme id-ctrl $testpath | awk '$1=="sn" {print $3}'`
-model_num=`nvme id-ctrl $testpath | awk '$1=="mn" {print $3, $4, $5, $6, $7}' | xargs | tr " " - | xargs`
-fw_rev=`nvme id-ctrl $testpath | awk '$1=="fr" {print $3}'`
-cap_Bytes=`nvme id-ctrl $testpath | awk '$1=="tnvmcap" {print $3}'`
-TB_multiplier=1000000000000
+vendor=`smartctl -i /dev/$SASDRIVE | awk '$1=="Vendor:" {print $2}'`
+product=`smartctl -i /dev/$SASDRIVE | awk '$1=="Product:" {print $2}'`
+product=`echo ${product/\//-}`
+serial=`sudo smartctl -i /dev/$SASDRIVE | awk '$1=="Serial" {print $3}'`
+fw_rev=`smartctl -i /dev/$SASDRIVE | awk '$1=="Revision:" {print $2}'`
+
 echo "Server: $server_model"
 echo "CPU: $cpu_model"
-echo "Serial Num: $serial_num"
-echo "Model Num: $model_num"
+echo "Vendor: $vendor"
+echo "Product: $product"
+echo "Serial: $serial"
 echo "FW_REV: $fw_rev"
-echo "Cap Bytes: $cap_Bytes"
-
-
-cap_TB=$(($cap_Bytes / $TB_multiplier))
-cap_TB=$((cap_TB+1))
-
-num_loops=2
-iosize=$(($cap_TB * $num_loops * 1000))
 
 date=$(date '+%Y%m%d')
 timestamp=$(date '+%H%M%S')
-result_dir=`echo "${model_num}_${serial_num}_${fw_rev}_${date}_${timestamp}_${cpu_model}_${server_model}" | xargs`
-telemetry_dir="Telemetry_Logs"
+result_dir=`echo "${product}_${serial}_${fw_rev}_${date}_${timestamp}_${cpu_model}_${server_model}" | xargs`
 run_output_dir="Run_Output"
-
 
 if [ -d ${result_dir} ]
 then
@@ -47,24 +39,6 @@ else
 fi
 
 cd ${result_dir}
-
-mkdir ${telemetry_dir}
-cd ${telemetry_dir}
-
-echo "Getting telemetry log prior to running workload started at"
-date
-nvme telemetry-log /dev/$NVMEDRIVE --output-file=CD6_telemetry_${date}_before_workload
-
-echo "Getting telemetry log prior to running workload completed at"
-date
-
-echo "Formatting drive started at"
-date
-nvme format /dev/$NVMEDRIVE --ses=1 --force
-echo "Formatting completed at"
-date
-
-cd ..
 
 mkdir ${run_output_dir}
 cd ${run_output_dir}
@@ -91,33 +65,33 @@ rd_wr_perc=(0 30 50 70 100)
 
 
 # RANDOM BS WORKLOAD ONLY
-#for bs in "${rnd_block_size[@]}"; do
+for bs in "${rnd_block_size[@]}"; do
 
-#echo "Random preconditioning for bs=${bs} started at"
-#date
-#echo "workload:fio --direct=1 --rw=randwrite  --bs=${bs} --iodepth=256 --ioengine=${ioeng} --numjobs=1 --norandommap=1 --randrepeat=0 --name=Ran_precondition_bs${bs}_qd256_t1 --group_reporting --filename=/dev/$NVMEDRIVE  --output-format=terse --loops=3"
-#fio --direct=1 --rw=randwrite  --bs=${bs} --iodepth=256 --ioengine=${ioeng} --numjobs=1 --norandommap=1 --randrepeat=0 --name=Ran_precondition_bs${bs}_qd256_t1 --group_reporting --filename=/dev/$NVMEDRIVE  --output-format=terse --loops=3
-#echo "workload independent preconditioning done at"
-#date
+echo "Random preconditioning for bs=${bs} started at"
+date
+echo "workload:fio --direct=1 --rw=randwrite  --bs=${bs} --iodepth=256 --ioengine=${ioeng} --numjobs=1 --norandommap=1 --randrepeat=0 --name=Ran_precondition_bs${bs}_qd256_t1 --group_reporting --filename=/dev/$SASDRIVE  --output-format=terse --loops=3"
+fio --direct=1 --rw=randwrite  --bs=${bs} --iodepth=256 --ioengine=${ioeng} --numjobs=1 --norandommap=1 --randrepeat=0 --name=Ran_precondition_bs${bs}_qd256_t1 --group_reporting --filename=/dev/$SASDRIVE  --output-format=terse --loops=3
+echo "workload independent preconditioning done at"
+date
 
-#for perc in "${rd_wr_perc[@]}"; do 
+for perc in "${rd_wr_perc[@]}"; do 
 
-#rd_perc=${perc}
-#wr_perc="$((100-${rd_perc}))"
+rd_perc=${perc}
+wr_perc="$((100-${rd_perc}))"
 
-#for t in "${rnd_threads[@]}"; do
+for t in "${rnd_threads[@]}"; do
 
-#echo "Random Mixed ${rd_perc}% Read ${wr_perc}% Write bs=${bs} t${t} qd${t}"
-#date
-#echo "fio --time_based --runtime=300 --output-format=${run_type} --direct=1 --buffered=0 --rw=randrw --rwmixread=${rd_perc} --rwmixwrite=${wr_perc} --bs=${bs} --iodepth=${t} --ioengine=${ioeng} --numjobs=${t} --norandommap=1 --randrepeat=0 --group_reporting --percentile_list=${perc_list} --name=randmixedread${rd_perc}write${wr_perc}_${ioeng}_t${t}_qd${t}_bs${bs} --filename=/dev/$NVMEDRIVE --output=${result_dir}-randmixedread${rd_perc}write${wr_perc}-bs${bs}-threads${t}-depth${t}"
-#fio --time_based --runtime=300 --output-format=${run_type} --direct=1 --buffered=0 --rw=randrw --rwmixread=${rd_perc} --rwmixwrite=${wr_perc} --bs=${bs} --iodepth=${t} --ioengine=${ioeng} --numjobs=${t} --norandommap=1 --randrepeat=0 --group_reporting --percentile_list=${perc_list} --name=randmixedread${rd_perc}write${wr_perc}_${ioeng}_t${t}_qd${t}_bs${bs} --filename=/dev/$NVMEDRIVE --output=${result_dir}_randmixedread${rd_perc}write${wr_perc}-bs${bs}-threads${t}-depth${t}
-#date
+echo "Random Mixed ${rd_perc}% Read ${wr_perc}% Write bs=${bs} t${t} qd${t}"
+date
+echo "fio --time_based --runtime=300 --output-format=${run_type} --direct=1 --buffered=0 --rw=randrw --rwmixread=${rd_perc} --rwmixwrite=${wr_perc} --bs=${bs} --iodepth=${t} --ioengine=${ioeng} --numjobs=${t} --norandommap=1 --randrepeat=0 --group_reporting --percentile_list=${perc_list} --name=randmixedread${rd_perc}write${wr_perc}_${ioeng}_t${t}_qd${t}_bs${bs} --filename=/dev/$SASDRIVE --output=${result_dir}-randmixedread${rd_perc}write${wr_perc}-bs${bs}-threads${t}-depth${t}"
+fio --time_based --runtime=300 --output-format=${run_type} --direct=1 --buffered=0 --rw=randrw --rwmixread=${rd_perc} --rwmixwrite=${wr_perc} --bs=${bs} --iodepth=${t} --ioengine=${ioeng} --numjobs=${t} --norandommap=1 --randrepeat=0 --group_reporting --percentile_list=${perc_list} --name=randmixedread${rd_perc}write${wr_perc}_${ioeng}_t${t}_qd${t}_bs${bs} --filename=/dev/$SASDRIVE --output=${result_dir}_randmixedread${rd_perc}write${wr_perc}-bs${bs}-threads${t}-depth${t}
+date
 
-#done 
+done 
 
-#done
+done
 
-#done
+done
 
 
 # SEQUENTIAL BS WORKLOAD ONLY
@@ -125,8 +99,8 @@ for bs in "${seq_block_size[@]}"; do
 
 echo "Sequential preconditioning for bs=${bs} started at"
 date
-echo "workload:fio --direct=1 --rw=write  --bs=${bs} --iodepth=256 --ioengine=${ioeng} --numjobs=1 --norandommap=1 --randrepeat=0 --name=Seq_precondition_bs${bs}_qd256_t1 --group_reporting --filename=/dev/$NVMEDRIVE  --output-format=terse --loops=3"
-fio --direct=1 --rw=write  --bs=${bs} --iodepth=256 --ioengine=${ioeng} --numjobs=1 --norandommap=1 --randrepeat=0 --name=Seq_precondition_bs${bs}_qd256_t1 --group_reporting --filename=/dev/$NVMEDRIVE  --output-format=terse --loops=3
+echo "workload:fio --direct=1 --rw=write  --bs=${bs} --iodepth=256 --ioengine=${ioeng} --numjobs=1 --norandommap=1 --randrepeat=0 --name=Seq_precondition_bs${bs}_qd256_t1 --group_reporting --filename=/dev/$SASDRIVE  --output-format=terse --loops=3"
+fio --direct=1 --rw=write  --bs=${bs} --iodepth=256 --ioengine=${ioeng} --numjobs=1 --norandommap=1 --randrepeat=0 --name=Seq_precondition_bs${bs}_qd256_t1 --group_reporting --filename=/dev/$SASDRIVE  --output-format=terse --loops=3
 echo "workload independent preconditioning done at"
 date
 
@@ -139,8 +113,8 @@ for t in "${seq_threads[@]}"; do
 
 echo "Sequential Mixed ${rd_perc}% Read ${wr_perc}% Write bs=${bs} t${t} qd${t}"
 date
-echo "fio --time_based --runtime=300 --output-format=${run_type} --direct=1 --buffered=0 --rw=rw --rwmixread=${rd_perc} --rwmixwrite=${wr_perc} --bs=${bs} --iodepth=${t} --ioengine=${ioeng} --numjobs=${t} --norandommap=1 --randrepeat=0 --group_reporting --percentile_list=${perc_list} --name=seqmixedread${rd_perc}write${wr_perc}_${ioeng}_t${t}_qd${t}_bs${bs} --filename=/dev/$NVMEDRIVE --output=${result_dir}-seqmixedread${rd_perc}write${wr_perc}_-bs${bs}-threads${t}-depth${t}"
-fio --time_based --runtime=300 --output-format=${run_type} --direct=1 --buffered=0 --rw=rw --rwmixread=${rd_perc} --rwmixwrite=${wr_perc} --bs=${bs} --iodepth=${t} --ioengine=${ioeng} --numjobs=${t} --norandommap=1 --randrepeat=0 --group_reporting --percentile_list=${perc_list} --name=seqmixedread${rd_perc}write${wr_perc}_${ioeng}_t${t}_qd${t}_bs${bs} --filename=/dev/$NVMEDRIVE --output=${result_dir}_seqmixedread${rd_perc}write${wr_perc}-bs${bs}-threads${t}-depth${t}
+echo "fio --time_based --runtime=300 --output-format=${run_type} --direct=1 --buffered=0 --rw=rw --rwmixread=${rd_perc} --rwmixwrite=${wr_perc} --bs=${bs} --iodepth=${t} --ioengine=${ioeng} --numjobs=${t} --norandommap=1 --randrepeat=0 --group_reporting --percentile_list=${perc_list} --name=seqmixedread${rd_perc}write${wr_perc}_${ioeng}_t${t}_qd${t}_bs${bs} --filename=/dev/$SASDRIVE --output=${result_dir}-seqmixedread${rd_perc}write${wr_perc}_-bs${bs}-threads${t}-depth${t}"
+fio --time_based --runtime=300 --output-format=${run_type} --direct=1 --buffered=0 --rw=rw --rwmixread=${rd_perc} --rwmixwrite=${wr_perc} --bs=${bs} --iodepth=${t} --ioengine=${ioeng} --numjobs=${t} --norandommap=1 --randrepeat=0 --group_reporting --percentile_list=${perc_list} --name=seqmixedread${rd_perc}write${wr_perc}_${ioeng}_t${t}_qd${t}_bs${bs} --filename=/dev/$SASDRIVE --output=${result_dir}_seqmixedread${rd_perc}write${wr_perc}-bs${bs}-threads${t}-depth${t}
 date
 
 done
@@ -160,14 +134,6 @@ cd ..
 #done 
 #mv output.csv ${run_output_dir}/output.csv
 
-cd ${telemetry_dir}
-
-echo "Getting telemetry log after running workload started at"
-date
-nvme telemetry-log /dev/$NVMEDRIVE --output-file=CD6_telemetry_${date}_after_workload
-
-echo "Getting telemetry log after running workload completed at"
-date
 
 cd ..
 cd ..
