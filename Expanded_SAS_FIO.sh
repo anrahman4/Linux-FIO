20ad96c4ca271fbace19a510e96e95bc3f0cff51
anrahman4 <70340275+anrahman4@users.noreply.github.com>
2021-03-30 15:59:33 -0700

    Update Expanded_SAS_FIO.sh

--- Expanded_SAS_FIO.sh
+++ Expanded_SAS_FIO.sh
@@ -29,6 +29,8 @@ date=$(date '+%Y%m%d')
 timestamp=$(date '+%H%M%S')
 result_dir=`echo "${product}_${serial}_${fw_rev}_${date}_${timestamp}_${cpu_model}_${server_model}" | xargs`
 run_output_dir="Run_Output"
+rand_output_dir="Random"
+seq_output_dir="Sequential"
 
 if [ -d ${result_dir} ]
 then
@@ -42,6 +44,9 @@ cd ${result_dir}
 
 mkdir ${run_output_dir}
 cd ${run_output_dir}
+
+mkdir ${rand_output_dir}
+mkdir ${seq_output_dir}
 
 #ioengine
 ioeng="libaio"
@@ -63,9 +68,13 @@ perc_list="99:99.9:99.99:99.999:99.9999:100"
 #read write percentages
 rd_wr_perc=(0 30 50 70 100)
 
+cd ${rand_output_dir}
 
 # RANDOM BS WORKLOAD ONLY
 for bs in "${rnd_block_size[@]}"; do
+
+mkdir ${bs}
+cd ${bs}
 
 echo "Random preconditioning for bs=${bs} started at"
 date
@@ -101,11 +110,19 @@ done
 
 done
 
+cd ..
+
 done
 
+cd .. 
+
+cd ${seq_output_dir}
 
 # SEQUENTIAL BS WORKLOAD ONLY
 for bs in "${seq_block_size[@]}"; do
+
+mkdir ${bs}
+cd ${bs}
 
 echo "Sequential preconditioning for bs=${bs} started at"
 date
@@ -131,12 +148,9 @@ done
 
 done
 
-done
-
-
-
-
 cd ..
+
+done
 
 #for file in ${run_output_dir}/*
 #do
