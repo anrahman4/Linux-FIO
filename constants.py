9649fa84ba3aeaca08ffc4062ea7472f9cd17c39
anrahman4 <70340275+anrahman4@users.noreply.github.com>
2021-07-26 15:35:36 -0700

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
 DRIVER = "{ODBC Driver 17 for SQL Server}"
-SERVER = "192.168.20.233"
-DATABASE_NAME = "SSD_Testing"
+SERVER = "10.90.133.207"
+DATABASE_NAME = "APL-Performance"
 USERNAME = "labuser"
 PASSWORD = "Kioxia123"
