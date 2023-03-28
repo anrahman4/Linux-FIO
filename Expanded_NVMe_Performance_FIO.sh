a90047b09321e8cd972917ffd63c6d24782c4788
anrahman4 <70340275+anrahman4@users.noreply.github.com>
2023-03-28 15:07:59 -0700

    Create Expanded_NVMe_Performance_FIO.sh

--- Expanded_NVMe_Performance_FIO.sh
+++ Expanded_NVMe_Performance_FIO.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Usage: ./NVMe_Performance_FIO.sh <dev_device> <drive_alias> > output.txt 2>&1 & 
+# Usage: ./Expanded_NVMe_Performance_FIO.sh <dev_device> <drive_alias> > output.txt 2>&1 & 
 
 NVMEDRIVE=$1
 echo "Benchmark Drive: ${NVMEDRIVE}"
